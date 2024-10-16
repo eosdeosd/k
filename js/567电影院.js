b93dfa1f9dbcd635c46805cb531f7672f1896bd4
eosdeosd <97381204+eosdeosd@users.noreply.github.com>
2024-10-16 15:54:18 +0800

    Add files via upload

--- js/567电影院.js
+++ js/567电影院.js
@@ -25,23 +25,23 @@ var rule = {
 	},
 	searchUrl: '/search/**----------fypage---.html',
 	parse: 'https://jx.lasi.fun/blue/index.php?url=',
-       lazy: `js:
-		var html = JSON.parse(request(input).match(/r player_.*?=(.*?)</)[1]);
-		var url = html.url;
-		if (html.encrypt == '1') {
-			url = unescape(url)
-		} else if (html.encrypt == '2') {
-			url = unescape(base64Decode(url))
-		}
-		if (/\\.m3u8|\\.mp4/.test(url)) {
-			input = {
-				jx: 0,
-				url: url,
-				parse: 0
-			}
-		} else {
-			input
-		}
-	`,
+       lazy: $js.toString(() => {
+        let jxUrl = 'https://jx.lasi.fun/blue/index.php?url=;
+fetch_params.headers.Referer = 'https://www.567dyy.com';
+// fetch_params.headers['User-Agent'] = 'Mozilla/5.0 (Linux; Android 6.0.1; OPPO R9s Plus Build/MMB29M; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/55.0.2883.91 Mobile Safari/537.36';
+fetch_params.headers['User-Agent'] = MOBILE_UA;
+try {
+    let html = request(jxUrl+input);
+    log(html);
+    realUrl=html.match(/source src="(.*?)"/)[1];
+    log('解析到真实播放地址:'+realUrl);
+}catch (e) {
+    log('解析发生错误:'+e.message);
+    realUrl = input;
+}
+
+return realUrl
+    }),
+   
 	搜索: muban.首图2.搜索2,
 }
